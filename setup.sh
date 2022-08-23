aa41a1fef134fd725e2c04dff069512bda6f37e0
Sanju Yadav <107977396+yadavaas@users.noreply.github.com>
2022-08-24 01:07:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 [theme]\n\
-primaryColor = "\#70f9b3"\n\
-backgroundColor = "\#0b2d4c"\n\
-secondaryBackgroundColor = "\#4c4747"\n\
-textColor = "\#ffffff"\n\
+primaryColor = \"#70f9b3\"\n\
+backgroundColor = \"#0b2d4c\"\n\
+secondaryBackgroundColor = \"#4c4747\"\n\
+textColor = \"#ffffff\"\n\
 " > ~/.streamlit/config.toml
